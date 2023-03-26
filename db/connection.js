66c7e6e8dda8ec520986bf256bd6b24cf8bc5400
osjarju <115750838+osjarju@users.noreply.github.com>
2023-03-26 10:32:04 -0700

    total functionality achieved

--- db/connection.js
+++ db/connection.js
@@ -2,10 +2,12 @@ const mysql = require("mysql2");
 
 const connection = mysql.createConnection({
   host: "localhost",
+  // Port
+  port: 3306,
   // Your username
   user: "root",
   // Your password
-  password: "",
+  password: "45H@peAK12",
   database: "employees"
 });
 
--- index.js
+++ index.js
@@ -1,3 +1,4 @@
+//const mysql = require("mysql");
 const { prompt } = require("inquirer");
 const logo = require("asciiart-logo");
 const db = require("./db");
