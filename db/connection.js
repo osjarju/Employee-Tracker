f3c34e7b105c04c6ebccb7e94a540c7c714be5e6
osjarju <115750838+osjarju@users.noreply.github.com>
2023-04-02 13:32:56 -0700

    commit

--- db/connection.js
+++ db/connection.js
@@ -1,14 +1,7 @@
 const mysql = require("mysql2");
 
 const connection = mysql.createConnection({
-  host: "localhost",
-  // Port
-  port: 3306,
-  // Your username
-  user: "root",
-  // Your password
-  password: "",
-  database: ""
+
 });
 
 connection.connect(function (err) {
